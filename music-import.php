d8711e18ad893b01b9e45fa5ab7a421aa9e594f7
Michael Patterson-Muir <michael.patterson-muir@bbc.co.uk>
2016-02-11 14:57:27 +0000

    Initial Commit

---
+++ music-import.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'http://httpbin.org',
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+]);
+
